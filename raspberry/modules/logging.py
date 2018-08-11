e4278f90120f67a3a26ac80390e5c69fb44e14d9
Enes Demirağ <demirag16@itu.edu.tr>
2018-08-11 23:26:18 +0300

    Add timestamp function

--- raspberry/modules/logging.py
+++ raspberry/modules/logging.py
@@ -4,3 +4,6 @@ logpad = open("log.txt", "w") # Open a document
 
 def log(msg):
     print(str(time.strftime("%H:%M:%S", time.localtime(time.time()))) + ": " + msg, file = logpad)
+
+def timestamp():
+    return time.strftime("%H:%M:%S", time.localtime(time.time())
