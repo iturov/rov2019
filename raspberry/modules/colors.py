623e66477370c6aa1f35fa8e1cd5313f09d8abd5
Enes Demirağ <demirag16@itu.edu.tr>
2018-08-11 20:45:42 +0300

    Add colored print module

---
+++ raspberry/modules/colors.py
@@ -0,0 +1,22 @@
+# Colored print functions
+
+def error(msg):
+    print("\033[91m" + msg + "\033[0m") # red
+
+def warn(msg):
+    print("\033[93m" + msg + "\033[0m") # yellow
+
+def success(msg):
+    print("\033[94m" + msg + "\033[0m") # blue
+
+def wait(msg):
+    print("\033[95m" + msg + "\033[0m") # purple
+
+def hack(msg):
+    print("\033[92m" + msg + "\033[0m") # green
+
+def bold(msg):
+    print("\033[1m" + msg + "\033[0m") # bold
+
+def underline(msg):
+    print("\033[4m" + msg + "\033[0m") # underline
